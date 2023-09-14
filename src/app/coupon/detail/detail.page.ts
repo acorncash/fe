215dc06281a3317a762fc64a89da700c84bf1c9e
hyeouna kim <kha@notakorea.com>
2023-09-15 07:50:41 +0900

    sweet alurt

--- src/app/coupon/detail/detail.page.ts
+++ src/app/coupon/detail/detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-detail',
@@ -15,6 +16,27 @@ export class DetailPage implements OnInit {
   async ngOnInit() {
     this.seq = this.route.snapshot.params['seq'];
     console.log(this.seq);
+  }
+
+  popup(){
+    Swal.fire({
+      text: "정말 결제 하시겠습니까?",
+      icon: 'question',
+      showCancelButton: true,
+      heightAuto: false,
+      confirmButtonText: "네",
+      cancelButtonText: "아니요",
+    }).then((result) =>
+    {
+        if (result.isConfirmed) {
+          Swal.fire({
+            text: "결제가 완료되었습니다",
+            icon: 'success',
+            heightAuto: false,
+        
+          })
+        }
+    });
   }
 
 }

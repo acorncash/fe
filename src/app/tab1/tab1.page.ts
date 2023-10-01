756912764049fcffbccd5fdf590382371ad54575
hyeouna kim <kha@notakorea.com>
2023-10-02 03:39:07 +0900

    sample data

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,23 +1,40 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { RestService } from 'src/app/api/rest.service';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
+  misionList: any;
   
   constructor(
     private router: Router,
+    private rest: RestService,
   ) {}
   
+  ionViewWillEnter() {
+    this.getMisionList();
+  }
+
   navAnswerDetail(seq:number) {  
     this.router.navigateByUrl('/answer-detail/' + seq);
   }
 
   navCouponDetail(seq:number) {  
     this.router.navigateByUrl('/coupon-detail/' + seq);
+  }
+
+  getMisionList() {
+    this.misionList = [];
+    this.rest.getMisionList("A").subscribe((data:any) => {
+      console.log(data);
+      this.misionList = data;
+    });
+    // this.rest.getMisionList("C").subscribe((data:any) => {
+    //   console.log(data);
+    //   this.misionList += data;
+    // });
   }
 }
---
+++ src/app/tab3/tab2.page.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestService } from 'src/app/api/rest.service';
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+
+export class Tab2Page {
+  selectedMenu = "answer";
+  answerMisionList: any;
+  captureMisionList: any;
+
+  constructor(
+    private router: Router,
+    private rest: RestService,
+  ) {}
+
+  async ngOnInit() {
+  }
+  
+  ionViewWillEnter() {
+    this.getMisionList();
+  }
+
+  clickMenu(keyword:string) {
+    this.selectedMenu = keyword;
+  }
+
+  navAnswerDetail(seq:number) {  
+    this.router.navigateByUrl('/answer-detail/' + seq);
+  }
+
+  navCaptureDetail(seq:number) {  
+    this.router.navigateByUrl('/capture-detail/' + seq);
+  }
+
+  getMisionList() {
+    this.answerMisionList = [];
+    this.rest.getMisionList("A").subscribe((data:any) => {
+      console.log(data);
+      this.answerMisionList = data;
+    });
+
+    this.captureMisionList = [];
+    this.rest.getMisionList("C").subscribe((data:any) => {
+      console.log(data);
+      this.captureMisionList = data;
+    });
+  }
+}

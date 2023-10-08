b346ee8297efa5024b6dd2b62132d39dcde8b1eb
hyeouna kim <kha@notakorea.com>
2023-10-08 23:36:25 +0900

    add :8100

--- src/app/join/login/login.page.ts
+++ src/app/join/login/login.page.ts
@@ -20,9 +20,9 @@ export class LoginPage implements OnInit {
     const REST_API_KEY = "b2f9c8bcb75d5dc1e65936bcffc386d1";
     let REDIRECT_URI = "";
 
-    REDIRECT_URI = "http://" + window.location.host +"/callback";
+    REDIRECT_URI = "http://" + window.location.host +":8100/callback";
 
-    const KAKAO_AUTH_URL = `http://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
     
     window.location.replace(`${KAKAO_AUTH_URL}`);
   }

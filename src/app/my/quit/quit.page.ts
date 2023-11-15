000a4377a8b09cc4ed742ba45329abfb0aa3ccad
hyeouna kim <kha@notakorea.com>
2023-11-16 08:38:54 +0900

    quit page add delete

--- src/app/my/quit/quit.page.ts
+++ src/app/my/quit/quit.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RestService } from 'src/app/api/rest.service';
 
 @Component({
   selector: 'app-quit',
@@ -6,13 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quit.page.scss'],
 })
 export class QuitPage implements OnInit {
+  userSeq: any;
+  constructor(
+    private rest: RestService,
+    private router: Router,
+  ) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    this.userSeq = localStorage.getItem("seq");
   }
 
-  notProvided() {
-    alert("서비스 준비중입니다");
+  userDelete() {
+    this.rest.postDelete(this.userSeq).subscribe((data:any) => {
+      console.log(data);
+      if(data.status == "Success") {
+        alert('탈퇴가 완료되었습니다')
+        this.router.navigateByUrl('/login');
+      } else {
+        alert('회원 탈퇴 중 문제가 생겼습니다')
+      }
+    });
   }
 }

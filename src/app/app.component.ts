bddb20ac7fe20d460b8114281cd1cd7b66ca0370
HONG\Administrator <hongj10>
2023-12-08 20:59:15 +0900

    fix : cordova 오류 수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,15 @@ declare let cordova: any;
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
   constructor() {
-    cordova.exec((result: any) => {
-      console.log('Success:', result); // 네이티브 메서드가 성공적으로 실행된 경우 결과를 처리합니다.
-    }, (error: any) => {
-      console.error('Error:', error); // 네이티브 메서드 실행 중에 오류가 발생한 경우 오류를 처리합니다.
-    }, 'testPlugin', 'coolMethod', [localStorage.getItem("seq")]);
+    document.addEventListener("deviceready", function() {
+      cordova.exec((result: any) => {
+        console.log('Success:', result);
+      }, (error: any) => {
+        console.error('Error:', error);
+      }, 'testPlugin', 'coolMethod', [localStorage.getItem("seq")]);
+    });
   }
 }
